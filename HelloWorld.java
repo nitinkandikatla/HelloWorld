58ad0ba490ab3ff1931b6352ef0f2691e6a0a8f6
nitinkandikatla <nitinkandikatla99@gmail.com>
2021-01-08 14:24:57 +0530

    ADDED : added functionality addtion of two number

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,10 @@ package javasamples;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld welcome to java");
+		System.out.println(add(1,2));
+	}
+	public static int add(int num1,int num2) {
+		return (num1+num2);
 	}
 
 }
